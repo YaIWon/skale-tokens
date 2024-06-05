854e42d0980bfd42b684c87fabe6e7dcee9b7ee0
TheGreatAxios <sawyer@dirtroad.dev>
2024-06-05 09:26:25 -0700

    Add Tokens

---
+++ contracts/ERC20/Europa/wCT1.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+import "../IMA/SkaleMappedERC20Token.sol";
+
+https://etherscan.io/address/0x514910771AF9Ca656af840dff83E8264EcF986CA#code
+contract WrappedCarToken1 is SkaleMappedERC20Token("Wrapped Car Token 1", "wCT1") {
+
+    function decimals() override public view returns (uint8) {
+        return 2;
+    }
+}
