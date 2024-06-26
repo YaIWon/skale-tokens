a7e90ea8f6d142e226c3b05ff630fcd3cfb072d1
TheGreatAxios <sawyer@dirtroad.dev>
2024-06-26 14:02:28 -0700

    Add .nvmrc & Fix Broken Links

--- contracts/ERC20/Europa/Link.sol
+++ contracts/ERC20/Europa/Link.sol
@@ -3,5 +3,5 @@ pragma solidity ^0.8.7;
 
 import "../IMA/SkaleMappedERC20Token.sol";
 
-https://etherscan.io/address/0x514910771AF9Ca656af840dff83E8264EcF986CA#code
+// https://etherscan.io/address/0x514910771AF9Ca656af840dff83E8264EcF986CA#code
 contract LinkToken is SkaleMappedERC20Token("ChainLink Token", "LINK") {}
--- contracts/ERC20/Europa/wCT1.sol
+++ contracts/ERC20/Europa/wCT1.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.7;
 
 import "../IMA/SkaleMappedERC20Token.sol";
 
-https://etherscan.io/address/0x514910771AF9Ca656af840dff83E8264EcF986CA#code
+// https://etherscan.io/address/0x514910771AF9Ca656af840dff83E8264EcF986CA#code
 contract WrappedCarToken1 is SkaleMappedERC20Token("Wrapped Car Token 1", "wCT1") {
 
     function decimals() override public view returns (uint8) {
