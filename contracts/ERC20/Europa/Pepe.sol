a8022eff26aa07fe4676f444accdaca07db5f7cc
TheGreatAxios <sawyer@dirtroad.dev>
2024-08-23 12:43:06 -0700

    Tweak Role Set + Add Pepe

---
+++ contracts/ERC20/Europa/Pepe.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+import "../IMA/SkaleMappedERC20Token.sol";
+
+// https://etherscan.io/address/0x6982508145454ce325ddbe47a25d4ec3d2311933#code
+contract PepeToken is SkaleMappedERC20Token("Pepe", "PEPE") {}
