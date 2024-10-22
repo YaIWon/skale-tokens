266e083a91f8362454e61496285d71b36041242b
TheGreatAxios <sawyer@dirtroad.dev>
2024-10-22 11:29:11 -0700

    Add rETH

---
+++ contracts/ERC20/Europa/RocketTokenRETH.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+import "../IMA/SkaleMappedERC20Token.sol";
+
+// https://etherscan.io/token/0xae78736cd615f374d3085123a210448e74fc6393#code
+contract RocketTokenRETH is SkaleMappedERC20Token("Rocket Pool ETH", "rETH") {}
