603fbaa80c1ddb8bcaf3f2a04fb1f3063f5e700e
TheGreatAxios <sawyer@dirtroad.dev>
2024-12-07 16:45:17 -0800

    Add Unipoly Token

---
+++ contracts/ERC20/Europa/Unipoly.sol
@@ -0,0 +1,6 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+import "../IMA/SkaleMappedERC20Token.sol";
+
+contract Unipoly is SkaleMappedERC20Token("Unipoly Coin", "UNP") {}
---
+++ contracts/ERC20/IMA/EuropaUNP.sol
@@ -0,0 +1,6 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+import "./SkaleMappedERC20Token.sol";
+
+contract EuropaUNP is SkaleMappedERC20Token("Europa UNP", "UNP") {}
