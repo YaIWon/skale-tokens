fc38151b450369da875b19ecbdd413e0aef32618
TheGreatAxios <sawyer@dirtroad.dev>
2024-12-07 16:49:20 -0800

    Fix Skivvy Symbol

--- contracts/ERC20/Europa/Skivvy.sol
+++ contracts/ERC20/Europa/Skivvy.sol
@@ -4,7 +4,8 @@ pragma solidity ^0.8.7;
 import "../IMA/SkaleMappedERC20Token.sol";
 
 // https://holesky.etherscan.io/token/0x46E0F6C2aeC8C48588a5a4d064d32b14Fa685f78
-contract Skivvy is SkaleMappedERC20Token("Skivvy", "$SKIVVY") {
+
+contract Skivvy is SkaleMappedERC20Token("Skivvy", "SKIVVY") {
     
     function decimals() override public view returns (uint8) {
         return 8;
